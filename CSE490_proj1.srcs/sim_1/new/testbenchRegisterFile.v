069e4fc8ae91a22d0b87e342ed9c80052d8902f4
kyleshut <kyleshut@buffalo.edu>
2021-04-05 01:40:11 -0400

    registerFile testbench update

--- CSE490_proj1.srcs/sim_1/new/testbenchRegisterFile.v
+++ CSE490_proj1.srcs/sim_1/new/testbenchRegisterFile.v
@@ -30,9 +30,9 @@ wire [7:0] read_data1;
 wire [7:0] read_data2;
 reg [7:0] registers [0:1];
 registerFile UUT(.clock(clock),.read_reg1(read_reg1), .read_reg2(read_reg2), .write_reg(write_reg), .write_data(write_data), .read_data1(read_data1), .read_data2(read_data2));
-always #5 clock=~clock;
+/*always #5 clock=~clock;*/
 initial
-begin   
+begin 
 clock = 0;  
 registers[0] = 8'b00001000;
 registers[1] = 8'b00001001;
@@ -40,8 +40,15 @@ read_reg1 = 0;
 read_reg2 = 1;
 write_reg = read_reg1;
 write_data = 8'b00000101;
+#5
+clock = 1;
+#5
+clock = 0; 
+write_reg = read_reg2;
+write_data = 8'b00000010;
+#5
+clock = 1; 
 end 
-
 always@(posedge clock)
 begin
 /*if (write_reg == 1)*/

21aa348f605f942be779c898ab7d47ed3b26777f
rishitparihar <54640451+rishitparihar@users.noreply.github.com>
2021-04-05 16:39:25 -0400

    Add files via upload

---
+++ CSE490_proj1.srcs/sources_1/new/Add.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05.04.2021 16:37:35
+// Design Name: 
+// Module Name: Add
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module adder(A, B, SUM);  
+input  [7:0] A;  
+input  [7:0] B;  
+output [7:0] SUM; 
+ 
+  assign SUM = A + B;  
+endmodule 
+

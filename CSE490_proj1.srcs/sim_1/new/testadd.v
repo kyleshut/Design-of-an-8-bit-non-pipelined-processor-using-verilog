23c25926eb5ba7a7c46b7aa0f6d3f9474171f124
rishitparihar <54640451+rishitparihar@users.noreply.github.com>
2021-04-14 21:06:57 -0400

    Add files via upload

--- CSE490_proj1.srcs/sim_1/new/testadd.v
+++ CSE490_proj1.srcs/sim_1/new/testadd.v
@@ -4,20 +4,23 @@
 
 
 module testadd();
-reg in1, in2;
-wire out;
+  
+  reg [7:0] IN1,IN2;
+  wire [7:0] SUM;
+  wire CO;
+  
+  adder inst(.A(IN1),.B(IN2),.SUM(SUM),.CO(CO));
+  
+  initial begin
+    
+    IN1 = 8'd0; IN2 = 8'd0;
 
-adder test_adder(.A(in1),.B(in2), .SUM(out));
+#50 IN1 = 8'd0;   IN2 = 8'd1;  
+#50 IN1 = 8'd1;   IN2 = 8'd2;
+#50 IN1 = 8'd3;   IN2 = 8'd4;
+#50 IN1 = 8'd7;   IN2 = 8'd8;
 
-initial begin
-in1 = 0;
-in2 = 0;
-#5
-in1 = 1;
-in2 = 0;
-#5;
-in1 = 1;
-in2 = 1;
-#5;
 end
+
+
 endmodule

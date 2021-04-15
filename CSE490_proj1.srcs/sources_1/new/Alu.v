19615774117166caf90730685e2d0a87cee0a641
kyleshut <kyleshut@buffalo.edu>
2021-04-14 21:17:23 -0400

    don't overwrite alu

--- CSE490_proj1.srcs/sources_1/new/Alu.v
+++ CSE490_proj1.srcs/sources_1/new/Alu.v
@@ -20,19 +20,25 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module ALU (input [7:0] reg1,
-			input [7:0] reg2,
-			input [2:0] ALUControl,
-			output reg [7:0] result
-			);
-	
-    always @(reg1 or reg2 or ALUControl)
+module Alu (reg1, reg2,ALUControl, result);
+  
+  input [7:0] reg1;
+  input [7:0] reg2;
+  input [2:0] ALUControl;
+  output reg [7:0] result;
+    always @(*)
 		begin
 			casex (ALUControl)
-				3'b011: result = reg1 + reg2;
-				3'b100: result = reg1 + reg2;
-				3'b101: result = reg1 - reg2;
+			    3'b000:
+			         result = reg1 + reg2;
+			    3'b001:
+			         result = reg1 + reg2;
+				3'b011:  
+					result = reg1 + reg2;
+				3'b100:
+				    result = reg1 + reg2;
+				3'b101:  
+					result = reg1 - reg2;
 			endcase
 		end
 endmodule
-
